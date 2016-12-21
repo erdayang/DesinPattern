ba1c52fb2c398b3e33ba9accb8a6d37800565bfb
xuanda007 <yxda2014@gmail.com>
2016-12-21 23:58:40 +0800

    update

---
+++ design/src/main/java/com/mu/yang/rpc/handler/HandlerChain.java
@@ -0,0 +1,9 @@
+package com.mu.yang.rpc.handler;
+
+import com.mu.yang.worker.AbstractWorkerChain;
+
+/**
+ * Created by xuanda007 on 2016/12/21.
+ */
+public class HandlerChain extends AbstractWorkerChain {
+}
